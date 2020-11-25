071cab0ffd73898b3eb2231239a0c7a7b385e5cd
TheWillBa <wbabincsak@gmail.com>
2020-11-25 18:13:32 -0500

    Can now modify variables in the GUI

--- src/SimulationApplicationGUI.java
+++ src/SimulationApplicationGUI.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -8,15 +10,12 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-import javax.xml.bind.JAXBPermission;
-import java.awt.*;
-import java.lang.reflect.Type;
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,14 +23,21 @@ import java.util.stream.Collectors;
 public class SimulationApplicationGUI extends Application {
 
     // TODO let user control these
-    private int _diameter = 108; //mm
-    private int _speed = 2; //mph
-    private int _numLasers = 9;
-    private int _spacing = 35;
-    private int _numEdges = 32;
+    private final Int _diameter = new Int(108); //mm
+    private final Int _speed = new Int(2); //mph
+    private final Int _numLasers = new Int(9);
+    private final Int _spacing = new Int(35);
+    private final Int _numEdges = new Int(32);
 
-    private int _whole = 100;
-    private int _broken = 100;
+    private final Int _whole = new Int(100);
+    private final Int _broken = new Int(100);
+
+    private static class Int{
+        int _value;
+        public Int(int v){
+            _value = v;
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -43,6 +49,7 @@ public class SimulationApplicationGUI extends Application {
 
         final HBox rootPanel = new HBox();
 
+        //#region Canvas Setup
         double canvasWidth = 500;
         double canvasHeight = 400;
 
@@ -51,7 +58,6 @@ public class SimulationApplicationGUI extends Application {
         Canvas canvas = new Canvas(canvasWidth, canvasHeight);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-
         Button imageButton = new Button("Visualize Configuration"){
             public void fire () {
                 drawVisuals(gc, canvasWidth, canvasHeight, generatorsList, collectorsList);
@@ -59,7 +65,45 @@ public class SimulationApplicationGUI extends Application {
             }
         };
 
+        //#endregion
+
         final VBox optionsPanel = new VBox();
+
+        HBox settingsRow1 = new HBox();
+        HBox settingsRow2 = new HBox();
+        HBox settingsRow3 = new HBox();
+
+
+
+        VBox diameterPanel = createVariableSetterPanel("Diameter", _diameter);
+        VBox speedPanel = createVariableSetterPanel("Speed (MPH)", _speed);
+        VBox numLasersPanel = createVariableSetterPanel("Num Lasers", _numLasers);
+        VBox spacingPanel = createVariableSetterPanel("Laser Spacing", _spacing);
+        VBox numEdgesPanel = createVariableSetterPanel("Num Edges", _numEdges);
+        VBox wholePanel = createVariableSetterPanel("Whole Target Samples", _whole);
+        VBox brokenPanel = createVariableSetterPanel("Broken Target Samples", _broken);
+
+        /*
+        Label diameterLabel = new Label("Diameter = " + _diameter);
+        TextField diameterTf = new TextField(Integer.toString(_diameter));
+        diameterPanel.getChildren().addAll(diameterLabel, diameterTf);
+        EventHandler<ActionEvent> diameterEvent = e -> {
+            try{
+            _diameter = Integer.parseInt(diameterTf.getText());
+            }
+            catch(NumberFormatException ex) {
+                // do nothing
+            }
+            diameterLabel.setText("Diameter = " + _diameter);
+            System.out.println(_diameter);
+        };
+        diameterTf.setOnAction(diameterEvent);*/
+
+        settingsRow1.getChildren().addAll(diameterPanel, speedPanel, numEdgesPanel);
+        settingsRow2.getChildren().addAll(numLasersPanel, spacingPanel);
+        settingsRow3.getChildren().addAll(wholePanel, brokenPanel);
+
+            //#region ListView selectors setup
             final HBox selectionPanel = new HBox();
             selectionPanel.setPadding(new Insets(15, 12, 15, 12));
             selectionPanel.setSpacing(10);
@@ -72,7 +116,9 @@ public class SimulationApplicationGUI extends Application {
                 analyzerPanel.getChildren().addAll(new Label("Data Analyzer"), analyzersList); // TODO add generators
 
             selectionPanel.getChildren().addAll(generatorPanel, collectorPanel, analyzerPanel);
+            //#endregion
 
+            //#region Run button setup
             final VBox buttonPanel = new VBox();
             buttonPanel.setPadding(new Insets(15, 12, 15, 12));
             buttonPanel.setSpacing(10);
@@ -83,14 +129,35 @@ public class SimulationApplicationGUI extends Application {
                             /* Add display items that this has to modify here */);
                 }
             };
+            //#endregion
 
         buttonPanel.getChildren().addAll(searchButton, resultsLabel);
-        optionsPanel.getChildren().addAll(selectionPanel, buttonPanel);
+        optionsPanel.getChildren().addAll(settingsRow1, settingsRow2, settingsRow3, selectionPanel, buttonPanel);
         imagePanel.getChildren().addAll(imageButton, canvas);
         rootPanel.getChildren().addAll(optionsPanel, imagePanel);
-
+            // TODO allow export results to a file
         primaryStage.setScene(new Scene(rootPanel, 1000, 500));
         primaryStage.show();
+    }
+
+    private VBox createVariableSetterPanel(String text, Int var){
+        VBox panel = new VBox();
+        Label label = new Label(text + " = " + var._value);
+        TextField tf = new TextField(Integer.toString(var._value));
+        panel.getChildren().addAll(label, tf);
+        EventHandler<ActionEvent> event = e -> {
+            try{
+                var._value = Integer.parseInt(tf.getText());
+            }
+            catch(NumberFormatException ex) {
+                // do nothing
+            }
+            label.setText(text + " = " + var._value);
+            System.out.println(var._value);
+        };
+        tf.setOnAction(event);
+
+        return panel;
     }
 
     private void runSimulation(ListView<String> generators, ListView<String> collectors, ListView<String> analyzers, Label resultsLabel){
@@ -107,10 +174,9 @@ public class SimulationApplicationGUI extends Application {
         BreakageClassifier classifier = new LaserBreakageClassifier(collector, analyzer);
         BreakageClassifierTester tester = new BreakageClassifierTester(classifier, generator);
 
-        TestStatistics stats = tester.test(_whole, _broken);
+        TestStatistics stats = tester.test(_whole._value, _broken._value);
         resultsLabel.setText(stats.toString());
     }
-
 
     /**
      * Converts from a List of Listables to a List of String objects.
@@ -118,8 +184,6 @@ public class SimulationApplicationGUI extends Application {
     private static List<String> getNames (List<? extends Listable> items) {
         return items.stream().map(Listable::getName).collect(Collectors.toList());
     }
-
-
 
     /**
      * Converts from a Collection to an ObservableList of Node objects.
@@ -130,14 +194,13 @@ public class SimulationApplicationGUI extends Application {
         return FXCollections.observableList(getNames(items));
     }
 
-
     private List<TargetGenerator> masterGeneratorList(){
         List<TargetGenerator> generators = new ArrayList<TargetGenerator>();
 
-        TargetGenerator generator0 = new RandomQuadrantGenerator(_numEdges, _diameter, _speed);
+        TargetGenerator generator0 = new RandomQuadrantGenerator(_numEdges._value, _diameter._value, _speed._value);
         generators.add(generator0);
 
-        TargetGenerator generator1 = new ManySmallPiecesGenerator(_numEdges, _diameter, _speed);
+        TargetGenerator generator1 = new ManySmallPiecesGenerator(_numEdges._value, _diameter._value, _speed._value);
         generators.add(generator1);
 
         // Add more options here
@@ -148,10 +211,10 @@ public class SimulationApplicationGUI extends Application {
     private List<LaserDataCollector> masterDataCollectorList(){
         List<LaserDataCollector> collectors = new ArrayList<>();
 
-        LaserDataCollector collector0 = new InterruptLaserDataCollector(_numLasers, _spacing);
+        LaserDataCollector collector0 = new InterruptLaserDataCollector(_numLasers._value, _spacing._value);
         collectors.add(collector0);
 
-        LaserDataCollector collector1 = new PollingLaserDataCollector(_numLasers, _spacing);
+        LaserDataCollector collector1 = new PollingLaserDataCollector(_numLasers._value, _spacing._value);
         collectors.add(collector1);
 
         // Add more options here
@@ -162,7 +225,7 @@ public class SimulationApplicationGUI extends Application {
     private List<LaserDataAnalyzer> masterDataAnalyzerList(){
         List<LaserDataAnalyzer> analyzers = new ArrayList<>();
 
-        LaserDataAnalyzer analyzer0 = new BasicLaserDataAnalyzer(_diameter, _spacing);
+        LaserDataAnalyzer analyzer0 = new BasicLaserDataAnalyzer(_diameter._value, _spacing._value);
         analyzers.add(analyzer0);
 
         // Add more options here
